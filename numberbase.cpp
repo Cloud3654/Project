327f3a6095759db2a1bcda2939a2db425813c08f
Cloud3654 <163625776+Cloud3654@users.noreply.github.com>
2025-05-31 09:28:56 +0900

    Create numberbase.cpp

---
+++ numberbase.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+struct NUM {
+    vector<int> nums(10);
+};
+vector<NUM> digits(5);
+void init() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= 9; j++) {
+            digits[i].nums[j] = j;
+        }
+    }
+}
+bool comp(pair<int, int> a, pair<int, int> b) { return a.first > b.first; }
+int choose() {
+    vector<pair<int, int>> length_pos;
+}
+void del() {
+    
+}
+int main() {
+    cout << "Initializing..." << endl;
+    init();
+    string cor;
+    while (cor != "sssss") {
+        
+    }
+}
